eee4a29dad5ccdf9518cd6adf20119e50aa05755
Erepb <erepb-89@yandex.ru>
2024-04-16 09:20:01 +0300

    removed menu buttons

--- common/bot_cmds_list.py
+++ common/bot_cmds_list.py
@@ -1,10 +1,10 @@
-from aiogram.types import BotCommand
-
-private = [
-    BotCommand(command='start', description='Начать работу'),
-    BotCommand(command='about', description='О боте'),
-    BotCommand(command='screenshots', description='Несколько скриншотов из игры'),
-    BotCommand(command='game', description='Об игре'),
-    BotCommand(command='factions', description='Почитать описание фракций'),
-    BotCommand(command='units', description='Просмотр списка юнитов'),
-]
+# from aiogram.types import BotCommand
+#
+# private = [
+#     BotCommand(command='start', description='Начать работу'),
+#     BotCommand(command='about', description='О боте'),
+#     BotCommand(command='screenshots', description='Несколько скриншотов из игры'),
+#     BotCommand(command='game', description='Об игре'),
+#     BotCommand(command='factions', description='Почитать описание фракций'),
+#     BotCommand(command='units', description='Просмотр списка юнитов'),
+# ]
